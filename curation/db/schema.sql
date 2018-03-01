634ce850cb8f56e155c703653f606d2ccdab211d
SCOTT SCHORN <smschorn@gmail.com>
2018-02-28 21:59:20 -0500

    created database structure

--- curation/db/schema.sql
+++ curation/db/schema.sql
@@ -4,15 +4,41 @@
 -- TODO: [5] modify the quotes table to comply with the seed as well
 -- TODO: [6] run both, the schema file and the seed
 
-\c quotes_db;
-DROP TABLE IF EXISTS quotes;
+\c curation_db;
 
-CREATE TABLE quotes (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  content TEXT,
-  author VARCHAR(255),
-  genre_type VARCHAR(255),
+  name VARCHAR(255),
   date_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX ON quotes (author);
+CREATE INDEX ON users (name);
+
+DROP TABLE IF EXISTS collections;
+
+CREATE TABLE collections (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  user_id INTEGER,
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON collections (name);
+CREATE INDEX ON collections (user_id);
+
+DROP TABLE IF EXISTS artworks;
+
+CREATE TABLE artworks (
+  id SERIAL PRIMARY KEY,
+  collection_id INTEGER,
+  title VARCHAR(255),
+  object_id VARCHAR(255),
+  image_url VARCHAR(255),
+  date_created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON artworks (title);
+CREATE INDEX ON artworks (collection_id);
+CREATE INDEX ON artworks (object_id);
